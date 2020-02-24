e5d3b83551222dfd518adeb45f7fb6aaecbb738e
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-02-24 11:18:03 -0300

    Add some dependencies

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,13 +12,153 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          GoogleMap(
-              initialCameraPosition: CameraPosition(
-                  target: LatLng(-21.934350, -42.608710), zoom: 20))
-        ],
-      ),
+      body: Map(),
     );
+  }
+}
+
+class Map extends StatefulWidget {
+  Map({Key key}) : super(key: key);
+
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  GoogleMapController mapController;
+  static const _initialPosition = LatLng(-21.934350, -42.608710);
+  LatLng _lastPosition = _initialPosition;
+
+  final Set<Marker> _markers = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          initialCameraPosition:
+              CameraPosition(target: _initialPosition, zoom: 10.0),
+          onMapCreated: onCreated,
+          myLocationEnabled: true,
+          mapType: MapType.normal,
+          compassEnabled: true,
+          onCameraMove: _onCameraMove,
+          markers: _markers,
+        ),
+        Positioned(
+          top: 50.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 10,
+                    spreadRadius: 3)
+              ],
+            ),
+            child: TextField(
+              cursorColor: Colors.black,
+              //controller: appState.locationController,
+              decoration: InputDecoration(
+                icon: Container(
+                  margin: EdgeInsets.only(left: 20, top: 5),
+                  width: 10,
+                  height: 10,
+                  child: Icon(
+                    Icons.location_on,
+                    color: Colors.black,
+                  ),
+                ),
+                hintText: "pick up",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              ),
+            ),
+          ),
+        ),
+
+        Positioned(
+          top: 105.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 10,
+                    spreadRadius: 3)
+              ],
+            ),
+            child: TextField(
+              cursorColor: Colors.black,
+              //controller: appState.destinationController,
+              textInputAction: TextInputAction.go,
+              decoration: InputDecoration(
+                icon: Container(
+                  margin: EdgeInsets.only(left: 20, top: 5),
+                  width: 10,
+                  height: 10,
+                  child: Icon(
+                    Icons.local_taxi,
+                    color: Colors.black,
+                  ),
+                ),
+                hintText: "destination?",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              ),
+            ),
+          ),
+        ),
+        // Positioned(
+        //   top: 40,
+        //   right: 10,
+        //   child: FloatingActionButton(
+        //     onPressed: _onAddMarkerPressed,
+        //     tooltip: "add marker",
+        //     backgroundColor: Colors.black,
+        //     child: Icon(
+        //       Icons.add_location,
+        //       color: Colors.white,
+        //     ),
+        //   ),
+        // )
+      ],
+    );
+  }
+
+  void onCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    setState(() {
+      _lastPosition = position.target;
+    });
+  }
+
+  void _onAddMarkerPressed() {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(_lastPosition.toString()),
+          position: _lastPosition,
+          infoWindow: InfoWindow(title: "remember here", snippet: "good place"),
+          icon: BitmapDescriptor.defaultMarker));
+    });
   }
 }
