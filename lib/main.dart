536b912ea0b5a1574815abeab0a5077f8844a1d2
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-02-24 20:38:54 -0300

    Adicionado geolocator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:uberclone/screens/home.dart';
+import 'package:uberclone/states/app_state.dart';
+
+// void main() {
+//   return runApp(MultiProvider(
+//     providers: [
+//       ChangeNotifierProvider.value(
+//         value: AppState(),
+//       )
+//     ],
+//     child: MyApp(),
+//   ));
+// }
 
 void main() => runApp(MyApp());
 
@@ -13,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Uber clone'),
     );
   }
 }
